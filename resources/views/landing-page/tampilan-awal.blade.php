8ded66232b25bcb35164ad8e4c2d64207d7ceb34
chanisyandia <chanisyandiafz@gmail.com>
2023-07-06 10:39:05 +0700

    menambahkan tampilan utama landing page non user

---
+++ resources/views/landing-page/tampilan-awal.blade.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Tampilan Awal</title>
+
+    {{-- bootstrap 5 --}}
+    {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> --}}
+
+    {{-- bootstrap 4 --}}
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    {{-- mystyle css --}}
+    <link rel="stylesheet" type="text/css"href="assets/css/tampilan-awal.css">
+</head>
+<body>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand logo" href="#"><img src="assets/img/logo.png" width="150px"></a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+              <a class="nav-link active" aria-current="page" href="#" style="color: rgba(242, 100, 25, 1);">Beranda</a>
+              <a class="nav-link" href="#" style="color: rgba(242, 100, 25, 1);">Talent Hunter</a>
+              <a class="nav-link" href="#" style="color: rgba(242, 100, 25, 1);">Tips Kerja</a>
+              <a class="nav-link" href="#" style="color: rgba(242, 100, 25, 1);">Daftar Kandidat</a>
+              <a class="nav-link" href="#" style="color: rgba(242, 100, 25, 1);">Pasang Lowongan</a>
+              <button type="button" class="btn btn-primary" style="background-color: rgba(242, 100, 25, 1);" >Masuk</button>
+            </div>
+          </div>
+        </div>
+      </nav>
+
+      {{-- jumbotron --}}
+      <div class="jumbotron jumbotron-fluid" style="background-image: url('assets/img/bg-hero-1.png'); background-size:cover;">
+        <div class="container">
+          <h1 class="display-4 title-main">Tempatnya <br> Cari Kerja</h1>
+          <p class="lead child-main">Temukan loker terbaru se-Indonesia <br> dengan mudah!</p>
+        </div>
+      </div>
+
+      <div class="menu-main">
+        <div class="row">
+          <div class="col-sm"><div class="wadah">dfdfdfdf</div></div>
+          <div class="col-sm"><div class="wadah">dfdfdfdf</div></div>
+          <div class="col-sm"><div class="wadah">dfdfdfdf</div></div>
+          <div class="col-sm"><div class="wadah">dfdfdfdf</div></div>
+        </div>
+      </div>
+
+
+    {{-- bootstrap js 5 --}}
+      {{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script> --}}
+
+      {{-- bootstrap js  --}}
+      <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard');
+});
+
+Route::get('/tampilan-awal', function () {
+    return view('landing-page/tampilan-awal');
 });
