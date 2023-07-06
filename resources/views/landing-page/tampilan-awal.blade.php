80d8491a76a57b0908e80eeb6504fd3b8c6b9444
chanisyandia <chanisyandiafz@gmail.com>
2023-07-06 19:03:12 +0700

    memisahkan antara navbar dan footer dalam folder partials

--- resources/views/landing-page/tampilan-awal.blade.php
+++ resources/views/landing-page/tampilan-awal.blade.php
@@ -23,27 +23,8 @@
 
 <body>
 
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-            <a class="navbar-brand logo" href="#"><img src="assets/img/logo.png" width="150px"></a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
-                aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div class="navbar-nav">
-                    <a class="nav-link active font-underline" aria-current="page" href="#"
-                        style="color: rgba(242, 100, 25, 1);">Beranda</a>
-                    <a class="nav-link" href="#" style="color: rgba(242, 100, 25, 1);">Talent Hunter</a>
-                    <a class="nav-link" href="#" style="color: rgba(242, 100, 25, 1);">Tips Kerja</a>
-                    <a class="nav-link" href="#" style="color: rgba(242, 100, 25, 1);">Daftar Kandidat</a>
-                    <a class="nav-link" href="#" style="color: rgba(242, 100, 25, 1);">Pasang Lowongan</a>
-                    <button type="button" class="btn btn-primary"
-                        style="background-color: rgba(242, 100, 25, 1);">Masuk</button>
-                </div>
-            </div>
-        </div>
-    </nav>
+    @include('partials.navbar')
+    
 
     {{-- jumbotron --}}
     <div class="jumbotron jumbotron-fluid"
@@ -421,21 +402,7 @@
     <img src="assets/gif/loading-footer.gif" class="gif-loading">
 
     {{-- footer --}}
-    <footer>
-        <img src="assets/img/logo-areakerja-footer.png">
-        <p>Area Kerja adalah portal yang menyediakan informasi <br> mengenai lowongan kerja yang ada di Indonesia.</p>
-
-        <div class="social-media">
-            <ul>
-                <li><img src="assets/icon-img/icon-facebook.png"></li>
-                <li> <img src="assets/icon-img/icon-youtube.png"></li>
-                <li> <img src="assets/icon-img/icon-instagram.png"></li>
-                <li> <img src="assets/icon-img/icon-linkedin.png"></li>
-            </ul>
-        </div>
-
-        <p class="copyright">Copyright © AREAKERJA.com</p>
-    </footer>
+    @include('partials.footer')
 
 
     {{-- bootstrap js 5 --}}
